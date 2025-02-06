1d4c89ef4d4c824c7a46f58350110a21260105d2
ChrisUnivers <chrisonwindow@outlook.com>
2025-02-06 01:12:55 -0800

    simple rest api app using the mern stack

---
+++ mernstack-devExs/apl-project-fund/client/src/components/RecordList.jsx
@@ -0,0 +1,139 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const Record = (props) => (
+  <tr className="border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted">
+    <td className="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0">
+      {props.record.name}
+    </td>
+    <td className="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0">
+      {props.record.position}
+    </td>
+    <td className="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0">
+      {props.record.level}
+    </td>
+    <td className="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0">
+      <div className="flex gap-2">
+        <Link
+          className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 h-9 rounded-md px-3"
+          to={`/edit/${props.record._id}`}
+        >
+          Edit
+        </Link>
+        <button
+          className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 hover:text-accent-foreground h-9 rounded-md px-3"
+          color="red"
+          type="button"
+          onClick={() => {
+            props.deleteRecord(props.record._id);
+          }}
+        >
+          Delete
+        </button>
+      </div>
+    </td>
+  </tr>
+);
+
+export default function RecordList() {
+  const [records, setRecords] = useState([]);
+
+  // This method fetches the records from the database.
+  useEffect(() => {
+    async function getRecords() {
+      const response = await fetch(`http://localhost:5050/record/`);
+      if (!response.ok) {
+        const message = `An error occurred: ${response.statusText}`;
+        console.error(message);
+        return;
+      }
+      const records = await response.json();
+      setRecords(records);
+    }
+    getRecords();
+    return;
+  }, [records.length]);
+
+  // This method will delete a record
+  async function deleteRecord(id) {
+    await fetch(`http://localhost:5050/record/${id}`, {
+      method: "DELETE",
+    });
+    const newRecords = records.filter((el) => el._id !== id);
+    setRecords(newRecords);
+  }
+
+  // This method will map out the records on the table
+  function recordList() {
+    return records.map((record) => {
+      return (
+        <Record
+          record={record}
+          deleteRecord={() => deleteRecord(record._id)}
+          key={record._id}
+        />
+      );
+    });
+  }
+
+  // This following section will display the table with the records of individuals.
+  return (
+    <>
+      <h3 className="text-lg font-semibold p-4">Employee Records</h3>
+      <div className="border rounded-lg overflow-hidden">
+        <div className="relative w-full overflow-auto">
+          <table className="w-full caption-bottom text-sm">
+            <thead className="[&amp;_tr]:border-b">
+              <tr className="border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted">
+                <th className="h-12 px-4 text-left align-middle font-medium text-muted-foreground [&amp;:has([role=checkbox])]:pr-0">
+                  Name
+                </th>
+                <th className="h-12 px-4 text-left align-middle font-medium text-muted-foreground [&amp;:has([role=checkbox])]:pr-0">
+                  Position
+                </th>
+                <th className="h-12 px-4 text-left align-middle font-medium text-muted-foreground [&amp;:has([role=checkbox])]:pr-0">
+                  Level
+                </th>
+                <th className="h-12 px-4 text-left align-middle font-medium text-muted-foreground [&amp;:has([role=checkbox])]:pr-0">
+                  Action
+                </th>
+              </tr>
+            </thead>
+            <tbody className="[&amp;_tr:last-child]:border-0">
+              {recordList()}
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div class="pt-3">
+        <ul class="space-y-3">
+          <li class="flex items-center">
+              <div class="mt-10.0 flex min-w-xs items-start gap-x-1 rounded-xl bg-white p-6 shadow-none outline outline-gray-800 dark:bg-none dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
+                <div>
+                  <div class="flex items-center text-lg font-medium text-black dark:text-white">
+                    <p class="mr-3">ChitChat</p>
+                    <a href="#" class=""></a>
+                    <svg class="size-5.5 shrink-0" fill="none" stroke-linecap="square">
+                      <circle cx="11" cy="11" r="11" class="fill-sky-400/25" />
+                      <circle cx="11" cy="11" r="10.5" class="stroke-sky-400/25" />
+                      <path d="M8 11.5L10.5 14L14 8" class="stroke-sky-800 dark:stroke-sky-300" />
+                    </svg>
+                  </div>
+                  <p class="text-gray-500 dark:text-gray-400">You have a new message!</p>
+                  <p class="text-gray-500 dark:text-gray-400">You have a new message!</p>
+                </div>
+              </div>
+          </li>
+          <li class="flex items-center text-lg font-medium text-black 
+            dark:text-white">
+            <a href="https://tailwindcss.com/" class="flex min-w-xs rounded-xl bg-white p-6 shadow-none outline outline-black dark:bg-none dark:shadow-none dark:-outline-offset-1 dark:outline-white/10 ">
+              <p>Clickable div</p>
+            </a>
+          </li>
+        </ul>
+        
+      </div>
+      
+    </>
+  );
+}
